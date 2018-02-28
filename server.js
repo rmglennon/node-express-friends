68099d4b1f22d0b2ffdfc08ba54e6232cf581e7d
Rhonda Glennon <rhondaglennon@gmail.com>
2018-02-27 20:21:20 -0800

    create initial files and pseudocode

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
